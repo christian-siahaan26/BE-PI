49c3858f965ff58b6783bb986f1aee6a0e6aec28
ToDoList <christian123star@gmail.com>
2025-07-16 14:22:33 +0700

    Fix .gitignore

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,36 +1,36 @@
-import { PrismaClient } from "@prisma/client";
-import bcrypt from "bcrypt";
+// import { PrismaClient } from "@prisma/client";
+// import bcrypt from "bcrypt";
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 
-async function main() {
-  try {
-    const hashedPassword = await bcrypt.hash("password", 10);
+// async function main() {
+//   try {
+//     const hashedPassword = await bcrypt.hash("password", 10);
 
-    const admin = await prisma.user.upsert({
-      where: { email: "admin@gmail.com" },
-      update: {}, // Tidak diupdate kalau sudah ada
-      create: {
-        name: "Admin",
-        email: "admin@gmail.com",
-        password: hashedPassword,
-        role: "ADMIN",
-      },
-    });
-    console.log("✅ Lecturer user seeded:", admin);
-  } catch (error: any) {
-    if (error.code === "P2002" && error.meta?.target?.includes("name")) {
-      console.error("❌ Error: NIDK already exists in the database.");
-    } else {
-      console.error("❌ Error creating lecturer:", error);
-    }
-  }
-}
+//     const admin = await prisma.user.upsert({
+//       where: { email: "admin@gmail.com" },
+//       update: {},
+//       create: {
+//         name: "Admin",
+//         email: "admin@gmail.com",
+//         password: hashedPassword,
+//         role: "ADMIN",
+//       },
+//     });
+//     console.log("✅ Lecturer user seeded:", admin);
+//   } catch (error: any) {
+//     if (error.code === "P2002" && error.meta?.target?.includes("name")) {
+//       console.error("❌ Error: NIDK already exists in the database.");
+//     } else {
+//       console.error("❌ Error creating lecturer:", error);
+//     }
+//   }
+// }
 
-main()
-  .then(() => prisma.$disconnect())
-  .catch(async (e) => {
-    console.error("❌ Error seeding admin:", e);
-    await prisma.$disconnect();
-    process.exit(1);
-  });
+// main()
+//   .then(() => prisma.$disconnect())
+//   .catch(async (e) => {
+//     console.error("❌ Error seeding admin:", e);
+//     await prisma.$disconnect();
+//     process.exit(1);
+//   });
